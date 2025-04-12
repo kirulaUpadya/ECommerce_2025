6f8aaedc9b91a4eb1cffbb2528ffcaf05c9a304f
Kirula Upadya <kirulapadukka@gmail.com>
2025-04-12 23:52:56 +0530

    Initial Commit of Ecommerce

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('admin.index');
+    }
+
+    public function home()
+    {
+        return view('home.index');
+    }
+}
