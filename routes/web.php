062274669c0d371597ed6fc4abfd367646f8d4c7
Kirula Upadya <kirulapadukka@gmail.com>
2025-04-13 14:53:51 +0530

    Add category to the database.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 route::get('/', [HomeController::class, 'home']);
@@ -19,3 +20,7 @@ Route::middleware('auth')->group(function () {
 require __DIR__ . '/auth.php';
 
 route::get('admin/dashboard', [HomeController::class, 'index'])->middleware(['auth', 'admin']);
+
+route::get('view_category', [AdminController::class, 'view_category'])->middleware(['auth', 'admin']);
+
+route::post('add_category', [AdminController::class, 'add_category'])->middleware(['auth', 'admin']);
